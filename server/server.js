612c9fa2ad325607832d30573dbd442a2af0dca4
Dominic Tremblay <dominictremblay24@gmail.com>
2019-04-11 01:01:13 -0400

    reset for demo

--- server/server.js
+++ server/server.js
@@ -27,127 +27,12 @@ wss.broadcast = function broadcast(data) {
   });
 };
 
-const getCommandParts = message => {
-  const parts = message.split(' ');
-  const [cmd, ...content] = parts;
-
-  return {
-    cmd: cmd.replace('/', '').toLowerCase(),
-    content: content.join(' '),
-  };
-};
-
-const getPokemonCard = (content, cb) => {
-  const reqOptions = {
-    url: `https://api.pokemontcg.io/v1/cards?types=${content}`,
-    json: true,
-  };
-
-  request(reqOptions, (err, req, data) => {
-    if (err) {
-      console.log(err);
-    }
-
-    const { cards } = data;
-    const card = cards[Math.floor(Math.random() * cards.length)];
-    cb(card);
-  });
-};
-
-const connectClient = (client, nb) => {
-  clientId = uuidv4();
-  client.id = clientId;
-  clientList[clientId] = {
-    id: clientId,
-    username: `Anonymous${nb}`,
-  };
-
-  console.log(clientList);
-
-  const clientInfo = clientList[clientId];
-  clientInfo.type = 'incomingClientInfo';
-  console.log(clientInfo);
-  client.send(JSON.stringify(clientInfo));
-};
-
-const getUsernames = () =>
-  Object.values(clientList).map(client => client.username);
-
-const getGiphy = (content, cb) => {
-  reqOptions = {
-    url: `http://api.giphy.com/v1/gifs/search?api_key=${
-      process.env.GIPHY_API_KEY
-    }&q=${content}`,
-    json: true,
-  };
-
-  request(reqOptions, (err, req, gifs) => {
-    const { data } = gifs;
-    console.log(data);
-    cb(data[Math.floor(Math.random() * data.length)]);
-  });
-};
-
 wss.on('connection', ws => {
   console.log('Client connected');
   connectClient(ws, wss.clients.size);
 
   ws.on('message', message => {
     const receivedMsg = JSON.parse(message);
-
-    if (receivedMsg.content[0] === '/') {
-      const { cmd, content } = getCommandParts(receivedMsg.content);
-
-      receivedMsg.id = uuidv4();
-
-      switch (cmd) {
-        case 'me':
-          receivedMsg.type = 'meMessage';
-          receivedMsg.content = content;
-          wss.broadcast(JSON.stringify(receivedMsg));
-          break;
-
-        case 'shrug':
-          receivedMsg.type = 'shrugMessage';
-          receivedMsg.id = uuidv4();
-          receivedMsg.content = `${content} ¯\\_(ツ)_/¯`;
-          wss.broadcast(JSON.stringify(receivedMsg));
-          break;
-
-        case 'who':
-          receivedMsg.type = 'whoMessage';
-          receivedMsg.id = uuidv4();
-          receivedMsg.content = getUsernames();
-          ws.send(JSON.stringify(receivedMsg));
-          break;
-        case 'pokemon':
-          getPokemonCard(content, card => {
-            receivedMsg.content = {
-              name: card.name,
-              image: card.imageUrl,
-            };
-            receivedMsg.type = 'pokeMessage';
-            console.log(receivedMsg);
-            wss.broadcast(JSON.stringify(receivedMsg));
-          });
-          break;
-        case 'gif':
-          getGiphy(content, gif => {
-            receivedMsg.type = 'gifMessage';
-            receivedMsg.content = {
-              title: gif.title,
-              image: gif.images.original.url,
-            };
-            console.log(receivedMsg);
-            wss.broadcast(JSON.stringify(receivedMsg));
-          });
-          break;
-        default:
-          receivedMsg.type = 'errorMessage';
-          receivedMsg.content = 'Error: Unknown type of message';
-          ws.send(JSON.stringify(receivedMsg));
-      }
-    }
   });
   ws.on('close', () => console.log('Client disconnected'));
 });
