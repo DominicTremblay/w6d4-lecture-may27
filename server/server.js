097390ddfa80973847df05b8b781316ae61b8329
Dominic Tremblay <dominictremblay24@gmail.com>
2019-07-04 02:11:54 -0400

    server demo reset

--- server/server.js
+++ server/server.js
@@ -40,105 +40,12 @@ const connectClient = (client, nbClients) => {
   client.send(JSON.stringify(clientInfo));
 };
 
-const splitArguments = content => {
-  console.log('Content', content);
-  const splitContent = content.split(' ');
-
-  const [command, ...arguments] = splitContent;
-
-  return {
-    command: command.replace('/', ''),
-    arguments: arguments.join(' '),
-  };
-};
-
-const getGiphy = (searchContent, cb) => {
-  reqOptions = {
-    url: `http://api.giphy.com/v1/gifs/search?api_key=${
-      process.env.GIPHY_API_KEY
-    }&q=${searchContent}`,
-    json: true,
-  };
-
-  request(reqOptions, (err, resp, body) => {
-    const { data } = body;
-
-    const randomIndex = Math.floor(Math.random() * data.length);
-    cb(data[randomIndex]);
-  });
-};
-
-const getPokemon = (category, cb) => {
-  const reqOptions = {
-    url: `https://api.pokemontcg.io/v1/cards?types=${category}`,
-    json: true,
-  };
-  request(reqOptions, (err, resp, body) => {
-    const { cards } = body;
-
-    const randomIndex = Math.floor(Math.random() * cards.length);
-
-    cb(cards[randomIndex]);
-  });
-};
-
 wss.on('connection', wsClient => {
   console.log('Client connected');
   connectClient(wsClient, wss.clients.size);
 
   wsClient.on('message', message => {
     const receivedMsg = JSON.parse(message);
-
-    // if the first character is '/', we've got a command
-
-    if (receivedMsg.content[0] === '/') {
-      // separate the command and the arguements into 2 different variables
-      const { command, arguments } = splitArguments(receivedMsg.content);
-
-      // Reassign the content of received message
-      receivedMsg.content = arguments;
-
-      switch (command) {
-        case 'me':
-          receivedMsg.type = 'meMessage';
-          wsClient.send(JSON.stringify(receivedMsg));
-          break;
-        case 'shrug':
-          receivedMsg.type = 'shrugMessage';
-          wss.broadcast(JSON.stringify(receivedMsg));
-          break;
-        case 'gif':
-          receivedMsg.type = 'gifMessage';
-          getGiphy(receivedMsg.content, giphy => {
-            // Sending back the content as an object containing the title and url
-            receivedMsg.content = {
-              title: giphy.title,
-              imageUrl: giphy.images.original.url,
-            };
-
-            wss.broadcast(JSON.stringify(receivedMsg));
-          });
-
-          break;
-        case 'pokemon':
-          receivedMsg.type = 'pokeMessage';
-          getPokemon(receivedMsg.content, card => {
-            receivedMsg.content = {
-              name: card.name,
-              imageUrl: card.imageUrl,
-            };
-
-            wss.broadcast(JSON.stringify(receivedMsg));
-          });
-
-          break;
-        default:
-          // Send an error message back if unkown command
-          receivedMsg.type = 'errorMessage';
-          receivedMsg.content = 'Error: unknown command.';
-          wsClient.send(JSON.stringify(receivedMsg));
-      }
-    }
   });
   wsClient.on('close', () => console.log('Client disconnected'));
 });
