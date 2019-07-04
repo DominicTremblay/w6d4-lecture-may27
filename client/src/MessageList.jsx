99f5d0a4ea83b516c27e6f31474d63076f4a6432
Dominic Tremblay <dominictremblay24@gmail.com>
2019-07-04 11:27:01 -0400

    final demo may 27

--- client/src/MessageList.jsx
+++ client/src/MessageList.jsx
@@ -4,14 +4,74 @@ import Message from './Message.jsx';
 import Notification from './Notification.jsx';
 
 // Returns a Message or Notification component according to the
-// type of message. Instead of using a switch statement, the
-// function uses an object structure to store all the cases
+// type of message. Using a switch statement to return the right component
+
+const getMsgComponent = ({ id, username, content, type }) => {
+  switch (type) {
+    case 'meMessage':
+      return (
+        <Message
+          key={id}
+          username={username}
+          content={content}
+          styling="italic"
+        />
+      );
+      break;
+    case 'shrugMessage':
+      return (
+        <Message
+          key={id}
+          username={username}
+          content={`${content} ¯\\_(ツ)_/¯`}
+        />
+      );
+      break;
+    case 'pokeMessage':
+      return (
+        <Message
+          key={id}
+          username={username}
+          content={
+            <div>
+              <div>{content.name}</div>
+              <div>
+                <img src={content.imageUrl} alt={content.name} />
+              </div>
+            </div>
+          }
+        />
+      );
+      break;
+    case 'gifMessage':
+      return (
+        <Message
+          key={id}
+          username={username}
+          content={
+            <div>
+              <div>{content.title}</div>
+              <div>
+                <img src={content.imageUrl} alt={content.title} />
+              </div>
+            </div>
+          }
+        />
+      );
+      break;
+
+    default:
+      return <Notification key={id} content={content} />;
+  }
+};
 
 // Creating an array of components. Each component is customized according
 // to the type of message. Executing getMsgComponent to retrieve the right component
 
-const MessageList = props => {
-  return <main className="messages" />;
+const MessageList = ({ messages }) => {
+  const msgComponents = messages.map(msgObj => getMsgComponent(msgObj));
+
+  return <main className="messages">{msgComponents}</main>;
 };
 
 // Doing type checking on the props
--- server/server.js
+++ server/server.js
@@ -27,6 +27,7 @@ wss.broadcast = data => {
   });
 };
 
+// Closure. Returns a function that will get the next color in the array.
 const getColor = colors => {
   let index = 0;
 
@@ -38,6 +39,7 @@ const getColor = colors => {
   };
 };
 
+// Assign the return function of getColors
 const nextColor = getColor([
   'red',
   'blue',
@@ -60,6 +62,66 @@ const connectClient = (client, nbClients) => {
   client.send(JSON.stringify(clientInfo));
 };
 
+// splitting the command and the arguments separately
+const splitArguments = content => {
+  // desctructuring the array. Using the rest(...) operator to gather arguments
+  const [command, ...arguments] = content.split(' ');
+
+  // returning the command and the arguments
+  return {
+    command: command.replace('/', '').toLowerCase(),
+    arguments: arguments.join(' '),
+  };
+};
+
+// Making a request to the pokemon API to get a list of cards according to a category
+const getPokemonCard = (category, cb) => {
+  const reqOptions = {
+    url: `https://api.pokemontcg.io/v1/cards?types=${category}`,
+    json: true,
+  };
+
+  request(reqOptions, function(error, response, data) {
+    if (error) {
+      console.log('error:', error); // Print the error if one occurred
+    }
+
+    // destructuring cards to get the array
+    const { cards } = data;
+
+    // generating a random Index
+    const randomIndex = Math.floor(Math.random() * cards.length);
+
+    // Returning a random card
+    cb(cards[randomIndex]);
+  });
+};
+
+// Making a request to the giphy API to get a list of cards according to a category
+// You need to register and get your own api key
+const getGiphy = (searchContent, cb) => {
+  reqOptions = {
+    url: `http://api.giphy.com/v1/gifs/search?api_key=${
+      process.env.GIPHY_API_KEY
+    }&q=${searchContent}`,
+    json: true,
+  };
+
+  request(reqOptions, (error, response, images) => {
+    if (error) {
+      console.log('error:', error); // Print the error if one occurred
+    }
+
+    const { data } = images;
+
+    // generating a random Index
+    const randomIndex = Math.floor(Math.random() * data.length);
+
+    // return a random gif image
+    cb(data[randomIndex]);
+  });
+};
+
 wss.on('connection', wsClient => {
   console.log('Client connected');
   connectClient(wsClient, wss.clients.size);
@@ -67,7 +129,60 @@ wss.on('connection', wsClient => {
   wsClient.on('message', message => {
     const receivedMsg = JSON.parse(message);
 
-    console.log(receivedMsg);
+    // Adding the id to the message
+    receivedMsg.id = uuidv4();
+
+    // Is it a command such as /me ?
+    const isCommand = receivedMsg.content[0] === '/';
+
+    if (isCommand) {
+      // separating the command and the arguments parts
+      const { command, arguments } = splitArguments(receivedMsg.content);
+
+      switch (command) {
+      case 'me':
+        receivedMsg.type = 'meMessage';
+        receivedMsg.content = arguments;
+        wss.broadcast(JSON.stringify(receivedMsg));
+        break;
+      case 'shrug':
+        receivedMsg.type = 'shrugMessage';
+        receivedMsg.content = arguments;
+        wss.broadcast(JSON.stringify(receivedMsg));
+        break;
+      case 'pokemon':
+        receivedMsg.type = 'pokeMessage';
+        // request to the pokemon API
+        // provide a callback function
+        getPokemonCard(arguments, card => {
+          receivedMsg.content = {
+            name: card.name,
+            imageUrl: card.imageUrl,
+          };
+          wss.broadcast(JSON.stringify(receivedMsg));
+        });
+
+        break;
+      case 'gif':
+        receivedMsg.type = 'gifMessage';
+        // request to the giphy API
+        // provide a callback function
+        getGiphy(arguments, image => {
+          receivedMsg.content = {
+            title: image.title,
+            imageUrl: image.images.original.url,
+          };
+
+          wss.broadcast(JSON.stringify(receivedMsg));
+        });
+        break;
+      default:
+        // if we use a command not in the list
+        receivedMsg.type = 'errorMessage';
+        receivedMsg.content = 'Error. Unknown command';
+        wsClient.send(JSON.stringify(receivedMsg));
+      }
+    }
   });
   wsClient.on('close', () => console.log('Client disconnected'));
 });
